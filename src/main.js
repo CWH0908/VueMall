7f8c955d13c147a5013fe0936a9de4020f4ec88d
cwh0908 <2515145957@qq.com>
2019-06-25 14:36:20 +0800

    版本001

---
+++ src/main.js
@@ -0,0 +1,23 @@
+//main.js  作为项目的入口文件，在此处导入需要的文件
+
+
+import Vue from '../node_modules/vue/dist/vue.js' //导入vue
+
+import MintUI from 'mint-ui' //导入mint-ui
+import 'mint-ui/lib/style.css'
+
+import './lib/mui/css/mui.css'  //导入MUI
+import app from '../src/App.vue'
+
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+var vm=new Vue({
+	el:"#app",
+	data:{
+	},
+	components:{
+		'app':app,
+	},
+})
+
